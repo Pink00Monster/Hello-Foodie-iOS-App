1ea083d65823e7608847ba72438a6163b215d66f
Pink00Monster <vanillaWangyi@163.com>
2023-05-25 23:53:40 +0200

    recipe card

--- HelloFoodie/Views/Components/RecipeCard.swift
+++ HelloFoodie/Views/Components/RecipeCard.swift
@@ -11,7 +11,23 @@ struct RecipeCard: View {
     var recipe: Recipe
     
     var body: some View {
-        Text(recipe.name)
+        VStack {
+            AsyncImage(url: URL(string: recipe.image)) { image in
+                image
+            } placeholder: {
+                Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .foregroundColor(.white.opacity(0.3))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }
+        .frame(width: 19, height: 217, alignment: .top)
+        .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
+//        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+//        .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 10)
+
     }
 }
 
